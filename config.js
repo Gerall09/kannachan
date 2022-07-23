860451b3460632517a4cc07928b0eea74ae07293
Gerall09 <103030918+Gerall09@users.noreply.github.com>
2022-07-23 08:01:06 +0800

    Update config.js

--- config.js
+++ config.js
@@ -124,7 +124,7 @@ global.author = '                「 Geral  あ⁩ 」'
 
 
 /*============== LOGO ==============*/
-global.thumb = 'https://i.ibb.co/tB5KQdZ/20220710-135847.jpg' //Main Thumbnail
+global.thumb = 'https://telegra.ph/file/9960f328be97230d411ad.jpg' //Main Thumbnail
 global.imagebot = 'https://i.pinimg.com/originals/9b/3e/70/9b3e70d91a7db26d09ef1884049a3e71.jpg'
 global.giflogo = 'https://telegra.ph/file/a46ab7fa39338b1f54d5a.mp4'
 global.thumbs = ['https://hardianto.xyz/api/anime/random?sfw=wallpaper&apikey=hardianto']
